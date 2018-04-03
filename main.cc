b37c4fcf7c58f52ebbbf8048c3fe62e0a91e3aa5
Yohk0 <luc.menan@laposte.net>
2018-04-03 11:00:00 +0200

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+//un bon A* sa mère avec un bon heuristique son père
+//un bon graphe en extension son père
+//écriture de fichier
+//MCTS / autres (stochastique)
+//interruption ?
